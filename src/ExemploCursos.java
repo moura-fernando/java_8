82adb77446c1783cca65b79c9462114700bcf761
Fernando.Moura@Cieers.org.br <Fernando.Moura@cieers.org.br>
2019-01-16 21:16:08 -0200

    stream

---
+++ src/ExemploCursos.java
@@ -0,0 +1,108 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class ExemploCursos {
+	
+	public static void main(String[] args) {
+	
+		List<Curso> cursos = new ArrayList<>();
+		cursos.add(new Curso("Python", 45));
+		cursos.add(new Curso("JavaScript", 150));
+		cursos.add(new Curso("Java8", 113));
+		cursos.add(new Curso("C", 55));
+		
+		cursos.sort(Comparator.comparing(Curso::getAlunos));
+		
+
+//		cursos.forEach(System.out::println);   //IMPRIME O toString
+//		cursos.forEach(c -> System.out.println(c.getNome())); //Prara imprimir o nome só com lambda		
+		
+//		cursos.stream()
+//			.filter(c -> c.getAlunos() > 100)
+//			.forEach(c -> System.out.println(c.getNome()));
+		
+		cursos.stream()
+		.filter(c -> c.getAlunos() > 100)
+		.map(c -> c.getAlunos() )
+		.forEach(toral -> System.out.println(toral));
+		//ou Method references
+		cursos.stream()
+		.filter(c -> c.getAlunos() > 100)
+		.map(Curso::getAlunos)
+		.forEach(System.out::println);
+
+		int sum = cursos.stream()
+		.filter(c -> c.getAlunos() > 100 )
+		.mapToInt(Curso::getAlunos)
+		.sum();
+		
+		System.out.println(sum);
+	
+		
+		//OPTIONAL 
+		
+		Optional<Curso> optionalCurso = cursos.stream()
+			.filter(c -> c.getAlunos() > 10)
+			.findAny();
+		
+		 cursos.stream()
+			.filter(c -> c.getAlunos() > 10)
+			.findAny()
+			.ifPresent(c -> System.out.println(c.getNome()) );
+		
+		Curso curso = optionalCurso.orElse(null);
+		
+		System.out.println(curso.getNome());
+		
+		
+		//Como transformar um stream em uma Collection
+		
+		List<Curso> listaDeCursos =   cursos.stream()
+		.filter(c -> c.getAlunos() > 100)
+		.collect(Collectors.toList());
+		
+		listaDeCursos.forEach(c -> System.out.println("Curso de: " +c.getNome()));
+		
+		
+		 Map<String, Integer> collect = cursos 
+				.stream() 
+				.filter(c -> c.getAlunos() > 100) 
+				.collect(Collectors.toMap(c -> c.getNome(), c -> c.getAlunos()));
+		
+		System.out.println(collect);
+		
+		
+		
+
+	}
+}
+
+class Curso {
+	private String nome;
+	private int alunos;
+	
+	public Curso(String nome, int alunos) {
+		this.nome = nome;
+		this.alunos = alunos;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getAlunos() {
+		return alunos;
+	}
+
+	public void setAlunos(int alunos) {
+		this.alunos = alunos;
+	}
+}
--- src/OrdenaString.java
+++ src/OrdenaString.java
@@ -12,43 +12,25 @@ public class OrdenaString {
 		palavras.add("Gremio");
 		palavras.add("Futebol");
 		
-		Comparator<String> comparador = new ComparadorPorTamanho();
-		
 		//#Antes
 //		Collections.sort(palavras, comparador);
 		//#Java8    -->  sort é um método concreto dentro da interface List: Pode isso Arnaldo?
-		palavras.sort(comparador);
+	//1 palavras.sort(( s1,  s2) -> Integer.compare(s1.length(), s2.length()));
+	//2 palavras.sort(Comparator.comparing(s -> s.length()));
+	    palavras.sort(Comparator.comparing(String::length));
 		
 		//#Antes
 //		for (String p : palavras) {
 //			System.out.println(p);
 //		}
 		//#Java8
-//		palavras.forEach(p -> System.out.println(p));
-		palavras.forEach(new Consumer<String>() {
-
-			@Override
-			public void accept(String s) {
-				System.out.println(s);
-				
-			}
-		});
+		palavras.forEach(p -> System.out.println(p));
+		
+		//---------------------------
 	}
 }	
 	
-	class ComparadorPorTamanho implements Comparator<String> {
-
-		@Override
-		public int compare(String s1, String s2) {
-			if (s1.length()<s2.length()) {
-				return -1;
-			}
-			if (s1.length()>s2.length()) {
-				return 1;
-			}
-			return 0;
-		}
 		
-	}
+	
 
 
